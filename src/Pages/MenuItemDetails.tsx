31efff130df16a37b132b399aa7afdc07b46818b
Elmagn1f1co <steveamadi007@gmail.com>
2024-05-16 15:09:44 +0100

    Load menu item detail

--- src/Pages/MenuItemDetails.tsx
+++ src/Pages/MenuItemDetails.tsx
@@ -1,28 +1,69 @@
 import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+import { useGetMenuItemByIdQuery } from '../Apis/menuItemApi';
 
 function MenuItemDetails() {
+
+    const {menuItemId} = useParams();
+    const {data,isLoading} = useGetMenuItemByIdQuery(menuItemId);
+    const navigate = useNavigate();
   return (
     <div className='container pt-4 pt-md-5'>
-        <div className='row'>
+        {!isLoading? (<div className='row'>
             <div className='col-7'>
-                <h2 className='text-success'>NAME</h2>
+                <h2 className='text-success'>{data.result?.name}</h2>
                 <span>
                     <span className='badge text-bg-dark pt-2'
                     style={{height: "40px", fontSize: "20px"}}>
-                        CATEGORY
+                        {data.result?.category}
                     </span>
                 </span>
                 <span>
                     <span className='badge text-bg-light pt-2'
                     style={{height: "40px", fontSize: "20px"}}>
-                        SPECIAL TAG
+                        {data.result?.specialTag}
                     </span>
                 </span>
                 <p style={{fontSize: "20px"}} className='pt-2'>
-                    DESCRIPTION
+                {data.result?.description}
                 </p>
+                <span className="h3">${data.result?.price}</span> &nbsp;&nbsp;
+                <span className='pb-2 pb-3'
+                style={{border: "1px solid #333", borderRadius: "30px"}}>
+                    <i
+                    className='bi bi-dash p-1'
+                    style={{fontSize: "25px", cursor: "pointer"}}></i>
+                    <span className='h3 mt-3 px-3'>xx</span>
+                    <i
+                    className='bi bi-plus p-1'
+                    style={{fontSize: "25px", cursor:"pointer"}}></i>
+                </span>
+                <div className='row pt-4'>
+                    <div className='col-5'>
+                        <button className='btn btn-success form-control'>
+                            Add to Cart
+                        </button>
+                    </div>
+                    <div className="col-5">
+                        <button className='btn btn-secondary form-control'
+                        onClick={() => navigate(-1)}>
+                            Back to Home
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div className='col-5'>
+                <img src={data.result.image} 
+                width="100%"
+                style={{borderRadius: "50%"}}
+                alt="No content"></img>
+            </div>
+        </div>): (<div
+        className="d-flex justify-contenter-center"
+        style={{width: "100%"}}>
+        <div>Loading...</div>
+        </div>)}
+        
     </div>
   )
 }
